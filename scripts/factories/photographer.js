ea6789d22e573f0d7429dabd35ad6300fe5e3623
alaodriss <alaodriss@gmail.com>
2022-03-03 14:15:02 +0100

    creation page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait ,city , country, tagline,price} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,23 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        /* Ajouter "city+country" +taglien+price */
+        const span = document.createElement( 'span' );
+        span.textContent = city+' '+country;
+
+        const ParaGraphe = document.createElement( 'p' );
+        ParaGraphe.textContent = tagline;
+
+        const priceUnit = document.createElement( 'p' );
+        priceUnit.textContent = `${price}/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
+
+        article.appendChild(span);
+        article.appendChild(ParaGraphe);
+        article.appendChild(priceUnit);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
